a171030b844b7d2c438612d4a2c5d53e1d2fb885
lucasaf <ferreira.lucasalves@gmail.com>
2014-04-30 13:31:18 -0300

    First commit

---
+++ acheiimoveis/settings.py
@@ -0,0 +1,103 @@
+"""
+Django settings for acheiimoveis project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '2a11g!%bsapvbbmvlp@3-d!zrebm)vi=5n3-h*q555c(&b1z81'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'south',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'acheiimoveis.urls'
+
+WSGI_APPLICATION = 'acheiimoveis.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_sqlite3'),
+    }
+}
+
+'''
+DATABASES = {
+    'default': config(
+        'DATABASE_URL',
+        default='sqlite:///{0}'.format(os.path.join.(BASE_DIR, 'db.sqlite3')),
+        cast=db_url),
+}
+'''
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+
+
+try:
+    from local_settings import *
+except ImportError:
+    print 'Warning: local_settings not found'
