dff2fba082d841808e84b5e2bc37a94f13b24429
lucasaf <ferreira.lucasalves@gmail.com>
2014-08-19 13:56:56 -0300

    refactor and errors solutions

--- acheiimoveis/settings.py
+++ acheiimoveis/settings.py
@@ -9,11 +9,9 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import dj_database url
-#import os
-#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-#from unipath import Path
-#BASE_DIR = Path(__file__).parent
+import dj_database_url
+from unipath import Path
+BASE_DIR = Path(__file__).parent
 
 
 # Quick-start development settings - unsuitable for production
